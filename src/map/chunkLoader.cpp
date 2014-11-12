d8bff56eb90c4be5dc5519258bbf752e851897d3
Maxence Guichon <maxou70@gmail.com>
2014-11-12 19:05:04 +0100

    added chunkLoader classe

---
+++ src/map/chunkLoader.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   chunkLoader.cpp
+ * Author: maxence
+ * 
+ * Created on 12 novembre 2014, 19:02
+ */
+
+#include "chunkLoader.h"
+
+chunkLoader::chunkLoader() {
+}
+
+chunkLoader::~chunkLoader() {
+}
+
